d6be20720a9903372bf4b7dfbafdb18fd3997f32
Sylvain Filteau <cidsphere@gmail.com>
2011-12-10 15:52:19 -0500

    Added syntax highlighting to the theme

--- src/app/view/Page.js
+++ src/app/view/Page.js
@@ -18,8 +18,45 @@ Ext.define('Refer.view.Page', {
 			url: this.pagesPath + '/' + page,
 			success: function(response) {
 				me.update(response.responseText);
+
+				me.highlightCodeBloc(me.getEl().dom);
+
 				me.setLoading(false);
 			}
 		});
+	},
+
+	highlightCodeBloc: function(parentDom) {
+		SyntaxHighlighter.config.useScriptTags = false;
+		SyntaxHighlighter.defaults.toolbar = false;
+
+		var elems = Ext.DomQuery.select('pre code', parentDom);
+		for (var i = 0; i < elems.length; i++) {
+			var code = elems[i];
+			var brush = this.detectCodeType(code);
+			SyntaxHighlighter.highlight({
+				brush: brush
+			}, code);
+		}
+
+	},
+
+	detectCodeType: function(code) {
+		var text = Ext.String.htmlDecode(code.innerHTML);
+		if (Ext.String.trim(text).substr(0, 2) == "<?") {
+			return "php";
+		}
+
+		// Check if there is the line type=brushname
+		// If there is, then isolate brushname and then remove the line
+		var lines = text.split("\n");
+		if (lines.length && lines[0].indexOf("type=") == 0) {
+			var brush = lines[0].substr(5);
+			lines.shift();
+			code.innerHTML = Ext.String.htmlEncode(lines.join("\n"));
+			return brush;
+		}
+
+		return "html";
 	}
 });
